3655c3e844a16c2cb8c925cab9e9298e2d34662f
George Mwakio <mwakiogeorge90@gmail.com>
2026-01-20 23:51:53 +0300

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,13 @@
+lib = {
+    'name' : 'george mwakio',
+    'age' : 12,
+    'school' : 'USIU',
+    'married' : False
+    }
+
+# print(lib.keys())
+# print(lib.values())
+# print(lib)
+
+for key in lib.items():
+    print(f'{key}')
